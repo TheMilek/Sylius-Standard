f0b1ec4f54920c5cf1638d9b39c0b3bab0dc6c93
TheMilek <kam.grygierzec@gmail.com>
2025-05-08 15:01:30 +0200

    menu docs

---
+++ src/Menu/AdminProductVariantFormMenuListener.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Menu;
+
+use Sylius\Bundle\AdminBundle\Event\ProductVariantMenuBuilderEvent;
+
+final class AdminProductVariantFormMenuListener
+{
+    public function addItems(ProductVariantMenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $menu
+            ->addChild('media')
+            ->setAttribute('template', 'Admin/ProductVariant/Tab/_media.html.twig')
+            ->setLabel('Media')
+        ;
+    }
+}
